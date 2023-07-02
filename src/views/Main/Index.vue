d7a76889e1d10979afb151719e96ad4b6ebf5c67
Kenneth <kenneth9417@gmail.com>
2023-07-02 15:41:23 +0800

    deploy

--- src/views/Main/Index.vue
+++ src/views/Main/Index.vue
@@ -1,52 +1,37 @@
 <template>
-  <a-carousel autoplay>
-    <div><h3>1</h3></div>
-    <div><h3>2</h3></div>
-    <div><h3>3</h3></div>
-    <div><h3>4</h3></div>
-  </a-carousel>
   <div>
     <a-carousel autoplay>
-      <img class="" src="../../assets/image/banner1.jpg" alt="">
-      <img class="" src="../../assets/image/banner2.jpg" alt="">
-      <img class="" src="../../assets/image/banner4.jpg" alt="">
-      <img class="" src="../../assets/image/banner5.jpg" alt="">
-      <img class="" src="../../assets/image/banner6.jpg" alt="">
+      <img class="" src="../../assets/image/banner1.jpg" alt="" />
+      <img class="" src="../../assets/image/banner2.jpg" alt="" />
+      <img class="" src="../../assets/image/banner4.jpg" alt="" />
+      <img class="" src="../../assets/image/banner5.jpg" alt="" />
+      <img class="" src="../../assets/image/banner6.jpg" alt="" />
     </a-carousel>
   </div>
 
   <div class="flex flex-row justify-center">
-    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/LKzv6Qy_gxs" title="YouTube video player"
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube-nocookie.com/embed/LKzv6Qy_gxs"
+      title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen>
+      allowfullscreen
+    >
     </iframe>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
-<<<<<<< HEAD
-.ant-carousel :deep(.slick-slide) {
-  text-align: center;
-  height: 160px;
-=======
 /* For demo */
 .ant-carousel :deep(.slick-slide) {
   text-align: center;
   height: 300px;
->>>>>>> 9163566 (feat: add carousel and github page)
   line-height: 160px;
   background: #364d79;
   overflow: hidden;
 }
-<<<<<<< HEAD
-
-.ant-carousel :deep(.slick-slide h3) {
-  color: #fff;
-}
-=======
->>>>>>> 9163566 (feat: add carousel and github page)
 </style>
