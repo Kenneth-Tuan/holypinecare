3dab0b6769a6be5d961bf10d4c20c2c3fbf23b8f
Kenneth-Tuan <pokemenggou0.0@gmail.com>
2023-07-03 09:28:44 +0800

    refactor: header menu adjusting

--- src/views/Main/Index.vue
+++ src/views/Main/Index.vue
@@ -1,4 +1,10 @@
 <template>
+  <a-carousel autoplay>
+    <div><h3>1</h3></div>
+    <div><h3>2</h3></div>
+    <div><h3>3</h3></div>
+    <div><h3>4</h3></div>
+  </a-carousel>
   <div>
     <p>this is main</p>
   </div>
@@ -6,4 +12,16 @@
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ant-carousel :deep(.slick-slide) {
+  text-align: center;
+  height: 160px;
+  line-height: 160px;
+  background: #364d79;
+  overflow: hidden;
+}
+
+.ant-carousel :deep(.slick-slide h3) {
+  color: #fff;
+}
+</style>
