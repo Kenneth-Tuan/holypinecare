07719a90b4f0583391dad73caf5a7f1d76587aa2
Kenneth-Tuan <pokemenggou0.0@gmail.com>
2023-07-03 23:48:48 +0800

    feat: add product summary in main page

--- src/views/Main/Index.vue
+++ src/views/Main/Index.vue
@@ -1,22 +1,6 @@
 <template>
   <div>
-    <a-carousel autoplay>
-<<<<<<< HEAD
-      <img class="" src="../../assets/image/banner1.jpg" alt="">
-      <img class="" src="../../assets/image/banner2.jpg" alt="">
-      <img class="" src="../../assets/image/banner4.jpg" alt="">
-      <img class="" src="../../assets/image/banner5.jpg" alt="">
-      <img class="" src="../../assets/image/banner6.jpg" alt="">
-    </a-carousel>
-  </div>
-
-  <div class="flex flex-row justify-center">
-    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/LKzv6Qy_gxs" title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen>
-    </iframe>
-=======
+    <a-carousel autoplay class="mb-12">
       <img class="" src="../../assets/image/banner1.jpg" alt="" />
       <img class="" src="../../assets/image/banner2.jpg" alt="" />
       <img class="" src="../../assets/image/banner4.jpg" alt="" />
@@ -25,24 +9,52 @@
     </a-carousel>
   </div>
 
-  <div class="flex flex-row justify-between">
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube-nocookie.com/embed/LKzv6Qy_gxs"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
-    >
-    </iframe>
-    <!-- another video -->
->>>>>>> 139bf66feff96b9e914edbd5eb531450726f7272
+  <div class="pb-16">
+    <div class="page-title-wrapper">
+      <p class="page-title-content">產品介紹</p>
+      <div class="bg-gray-300 h-[1px] w-3/4"></div>
+    </div>
+    <div class="w-full flex flex-row justify-between gap-12">
+      <div
+        class="w-1/3 h-[300px] bg-gray-200/25 rounded-lg flex flex-col justify-end overflow-hidden"
+      >
+        <div class="bg-gray-200/75 h-1/4 w-full text-center">product</div>
+      </div>
+      <div
+        class="w-1/3 h-[300px] bg-gray-200/25 rounded-lg flex flex-col justify-end overflow-hidden"
+      >
+        <div class="bg-gray-200/75 h-1/4 w-full text-center">product</div>
+      </div>
+      <div
+        class="w-1/3 h-[300px] bg-gray-200/25 rounded-lg flex flex-col justify-end overflow-hidden"
+      >
+        <div class="bg-gray-200/75 h-1/4 w-full text-center">product</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="pb-16">
+    <div class="page-title-wrapper">
+      <p class="page-title-content">醫師開講</p>
+      <div class="bg-gray-300 h-[1px] w-3/4"></div>
+    </div>
+    <div class="flex flex-row justify-center">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/LKzv6Qy_gxs"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      >
+      </iframe>
+      <!-- another video -->
+    </div>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
 /* For demo */
