ba0c29cfdaed48b840a8695b75034552511ed63a
Kenneth <kenneth9417@gmail.com>
2023-07-13 00:41:38 +0800

    fix: adjust product list images route

--- src/views/Products/index.js
+++ src/views/Products/index.js
@@ -4,10 +4,10 @@ export const productList = [
     price: 0,
     id: "7-5",
     description: "-",
-    bannerImage: "/src/assets/products/低氘水1.jpg",
+    bannerImage: "../../assets/products/低氘水1.jpg",
     imageList: [
-      "/src/assets/products/低氘水1.jpg",
-      "/src/assets/products/低氘水2.jpg",
+      "../../assets/products/低氘水1.jpg",
+      "../../assets/products/低氘水2.jpg",
     ],
   },
   {
@@ -15,11 +15,11 @@ export const productList = [
     price: 12000,
     id: "7-4",
     description: "吸·喝·敷·噴 四種應用形態，做您24小時的全時健康助理。",
-    bannerImage: "/src/assets/products/氫水杯3.jpg",
+    bannerImage: "../../assets/products/氫水杯3.jpg",
     imageList: [
-      "/src/assets/products/氫水杯1.jpg",
-      "/src/assets/products/氫水杯2.jpg",
-      "/src/assets/products/氫水杯3.jpg",
+      "../../assets/products/氫水杯1.jpg",
+      "../../assets/products/氫水杯2.jpg",
+      "../../assets/products/氫水杯3.jpg",
     ],
   },
   {
@@ -27,11 +27,11 @@ export const productList = [
     price: 0,
     productId: "7-3",
     description: "氫心TM納米水氣泡臺上式氫水機。純物理混合，唇邊超高濃！",
-    bannerImage: "/src/assets/products/氫水機3.jpg",
+    bannerImage: "../../assets/products/氫水機3.jpg",
     imageList: [
-      "/src/assets/products/氫水機1.jpg",
-      "/src/assets/products/氫水機2.jpg",
-      "/src/assets/products/氫水機3.jpg",
+      "../../assets/products/氫水機1.jpg",
+      "../../assets/products/氫水機2.jpg",
+      "../../assets/products/氫水機3.jpg",
     ],
   },
   {
@@ -39,11 +39,11 @@ export const productList = [
     price: 46000,
     productId: "7-2",
     description: '換一種沐浴方式，打造一個"不會生鏽"的身體！',
-    bannerImage: "/src/assets/products/氫浴機3.jpg",
+    bannerImage: "../../assets/products/氫浴機3.jpg",
     imageList: [
-      "/src/assets/products/氫浴機1.jpg",
-      "/src/assets/products/氫浴機2.jpg",
-      "/src/assets/products/氫浴機3.jpg",
+      "../../assets/products/氫浴機1.jpg",
+      "../../assets/products/氫浴機2.jpg",
+      "../../assets/products/氫浴機3.jpg",
     ],
   },
   {
@@ -51,11 +51,11 @@ export const productList = [
     price: 120000,
     productId: "7-1",
     description: "最佳抗氧化劑，安全無毒性副作用。滲透細胞内層，修護受損細胞。",
-    bannerImage: "/src/assets/products/納諾巴伯3.jpg",
+    bannerImage: "../../assets/products/納諾巴伯3.jpg",
     imageList: [
-      "/src/assets/products/納諾巴伯1.jpg",
-      "/src/assets/products/納諾巴伯2.jpg",
-      "/src/assets/products/納諾巴伯3.jpg",
+      "../../assets/products/納諾巴伯1.jpg",
+      "../../assets/products/納諾巴伯2.jpg",
+      "../../assets/products/納諾巴伯3.jpg",
     ],
   },
 ];
